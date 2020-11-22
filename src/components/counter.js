4f1dd538b45f022907d0ab66e4c6c47a98fed8ee
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-11-22 21:30:37 +0500

    Update counter.js

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Text from './text'
+import Button from './button'
 
 const Counter = () => {
+    let [count, setCount] = useState(0);
+
     return(
         <div>
             <Text value={"Counter App"} 
                 style={{ fontSize: '24px', fontWeight: 'bold' }}
             />
-            <Text value={"Counter App"} 
+            <Text value={`Value of counter is ${count}`} 
                 style={{ fontSize: '24px', fontWeight: 'bold' }}
             />
+            <Button value={'Increament'} onClick={ () => setCount(count++) } />
+            <Button value={'Decreament'} onClick={ () => setCount(count--) } />
         </div>
     )
 }
+export default Counter;
