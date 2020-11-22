f3381ab08428b3a719e98ab1a89c8d0a7d27b6c2
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-11-23 02:59:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import Counter from './components/counter';
+import React from "react";
+import "./App.css";
+import Counter from "./components/counter";
 
 function App() {
   return (
