1d31dafd32f8f291773f15da3d64fb97942e093d
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-11-22 21:54:29 +0500

    Update counter.js

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,21 +1,32 @@
-import React, { useState } from 'react'
-import Text from './text'
-import Button from './button'
+import React, { useState } from "react";
+import Text from "./text";
+import Button from "./button";
+import "./counter.css";
 
 const Counter = () => {
-    let [count, setCount] = useState(0);
+  let [count, setCount] = useState(0);
 
-    return(
-        <div>
-            <Text value={"Counter App"} 
-                style={{ fontSize: '24px', fontWeight: 'bold' }}
-            />
-            <Text value={`Value of counter is ${count}`} 
-                style={{ fontSize: '24px', fontWeight: 'bold' }}
-            />
-            <Button value={'Increament'} onClick={ () => setCount(count++) } />
-            <Button value={'Decreament'} onClick={ () => setCount(count--) } />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Text
+        value={"Counter App"}
+        style={{ fontSize: "24px", fontWeight: "bold" }}
+      />
+      <Text
+        value={`Value of counter is ${count}`}
+        style={{ fontSize: "20px" }}
+      />
+      <Button
+        value={"Increament"}
+        onClick={() => setCount(count++)}
+        className="btn-inc"
+      />
+      <Button
+        value={"Decreament"}
+        onClick={() => setCount(count--)}
+        className="btn-dec"
+      />
+    </div>
+  );
+};
 export default Counter;
