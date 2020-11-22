767bcf401ac2db4c2c64ecdaf15e71ed9fb43b4f
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-11-23 02:57:59 +0500

    Update text.stories.js

--- src/stories/text.stories.js
+++ src/stories/text.stories.js
@@ -1,28 +1,19 @@
-import React from 'react'
-import Text from '../components/text'
+import React from "react";
+import Text from "../components/text";
 
 export default {
-    title: 'Text',
-    component: Text
-}
+  title: "Text",
+  component: Text,
+};
 
 export const textLg = () => (
-    <Text
-      value={"Large Text"}
-      style={{ fontSize: '36px' }}
-    />
+  <Text value={"Large Text"} style={{ fontSize: "36px" }} />
 );
-  
-  export const textMed = () => (
-    <Text
-      value={"Medium Text"}
-      style={{ fontSize: '28px' }}
-    />
+
+export const textMed = () => (
+  <Text value={"Medium Text"} style={{ fontSize: "28px" }} />
 );
-  
-  export const textSmall = () => (
-    <Text
-      value={"Small"}
-      style={{ fontSize: '20px' }}
-    />
+
+export const textSmall = () => (
+  <Text value={"Small"} style={{ fontSize: "20px" }} />
 );
