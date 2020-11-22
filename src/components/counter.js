3edb9a4a730c38260f8dbf58dc0bb98cc1df0501
rooshanahmed <rooshan.ahmed@hotmail.com>
2020-11-22 21:20:09 +0500

    Update counter.js

---
+++ src/components/counter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Text from './text'
+
+const Counter = () => {
+    return(
+        <div>
+            <Text value={"Counter App"} 
+                style={{ fontSize: '24px', fontWeight: 'bold' }}
+            />
+            <Text value={"Counter App"} 
+                style={{ fontSize: '24px', fontWeight: 'bold' }}
+            />
+        </div>
+    )
+}
